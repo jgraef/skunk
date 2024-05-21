4dfd7cf04820108e6f44f72e3ee6237c9a990f17
Janosch Gräf <janosch.graef@gmail.com>
2024-05-21 10:41:09 +0200

    refactored example code a bit

--- skunk-cli/src/app.rs
+++ skunk-cli/src/app.rs
@@ -25,20 +25,17 @@ use skunk::{
     util::CancellationToken,
 };
 use structopt::StructOpt;
+use tokio::net::TcpStream;
 
 use crate::config::Config;
 
 #[derive(Debug, StructOpt)]
 pub enum Command {
-    Ca,
-    Proxy {
-        /// File to store intercepted requests and responses in.
-        //file: Option<PathBuf>,
-
-        /// Name for this session.
-        //#[structopt(short, long = "name")]
-        //session_name: Option<String>,
-
+    Ca {
+        #[structopt(short, long)]
+        force: bool,
+    },
+    LogHttp {
         #[structopt(flatten)]
         socks: SocksArgs,
 
@@ -100,91 +97,118 @@ impl App {
 
     pub async fn run(&mut self, command: Command) -> Result<(), Error> {
         match command {
-            Command::Ca => {
-                // todo: if file exists, ask if we want to replace them
-                let ca = tls::Ca::generate().await?;
-                let key_file = self.config.path.join(&self.config.config.ca.key_file);
-                let cert_file = self.config.path.join(&self.config.config.ca.cert_file);
-                ca.save(&key_file, &cert_file)?;
-                tracing::info!("Key file saved to: {}", key_file.display());
-                tracing::info!("Cert file saved to: {}", cert_file.display());
+            Command::Ca { force } => {
+                self.generate_ca(force).await?;
             }
-            Command::Proxy { socks, target } => {
-                let ca = tls::Ca::open(
-                    self.config.path.join(&self.config.config.ca.key_file),
-                    self.config.path.join(&self.config.config.ca.cert_file),
-                )?;
-                let tls = tls::Context::new(ca).await?;
-
-                let filter = Arc::new(if target.is_empty() {
-                    tracing::info!("matching all flows");
-                    TargetFilter::All
-                }
-                else {
-                    tracing::info!("matching: {target:?}");
-                    TargetFilter::Set(target.into_iter().collect())
-                });
-
-                socks
-                    .builder()?
-                    .with_graceful_shutdown(cancel_on_ctrlc_or_sigterm())
-                    .with_proxy(fn_proxy(move |incoming: socks::Incoming, outgoing| {
-                        let tls = tls.clone();
-                        let filter = filter.clone();
-                        async move {
-                            let target_address = incoming.target_address();
-
-                            if filter.matches(target_address) {
-                                let span =
-                                    tracing::info_span!("connection", target = %target_address);
-
-                                let is_tls = target_address.port == 443;
-                                let (incoming, outgoing) =
-                                    tls.maybe_decrypt(incoming, outgoing, is_tls).await?;
-
-                                http::proxy(incoming, outgoing, |request, send_request| {
-                                    let span = tracing::info_span!(
-                                        parent: &span,
-                                        "request",
-                                        method = %request.method(),
-                                        uri = %request.uri()
-                                    );
-
-                                    async move {
-                                        // log request
-                                        tracing::info!(
-                                            parent: &span,
-                                            ">"
-                                        );
-
-                                        let response = send_request.send(request).await?;
-
-                                        // log response
-                                        tracing::info!(
-                                            parent: &span,
-                                            status = %response.status(),
-                                            "<"
-                                        );
-
-                                        Ok(response)
-                                    }
-                                })
-                                .await?;
-                            }
-                            else {
-                                Passthrough.proxy(incoming, outgoing).await?;
-                            };
-
-                            Ok::<_, skunk::Error>(())
-                        }
-                    }))
-                    .serve()
-                    .await?;
+            Command::LogHttp { socks, target } => {
+                self.log_http(socks, target).await?;
             }
         }
 
         Ok(())
     }
+
+    async fn generate_ca(&self, force: bool) -> Result<(), Error> {
+        let ca = tls::Ca::generate().await?;
+        let key_file = self.config.path.join(&self.config.config.ca.key_file);
+        let cert_file = self.config.path.join(&self.config.config.ca.cert_file);
+
+        if !force {
+            if key_file.exists() {
+                tracing::error!(key_file = %key_file.display(), "Key file already exists. Aborting. Run with --force to overwrite existing files.");
+                return Ok(());
+            }
+            if cert_file.exists() {
+                tracing::error!(cert_file = %cert_file.display(), "Cert file already exists. Aborting. Run with --force to overwrite existing files.");
+                return Ok(());
+            }
+        }
+
+        ca.save(&key_file, &cert_file)?;
+
+        tracing::info!(key_file = %key_file.display(), "Key file saved.");
+        tracing::info!(cert_file = %cert_file.display(), "Cert file saved.");
+
+        Ok(())
+    }
+
+    async fn log_http(&self, socks: SocksArgs, target: Vec<TcpAddress>) -> Result<(), Error> {
+        let ca = tls::Ca::open(
+            self.config.path.join(&self.config.config.ca.key_file),
+            self.config.path.join(&self.config.config.ca.cert_file),
+        )?;
+        let tls = tls::Context::new(ca).await?;
+
+        let filter = Arc::new(if target.is_empty() {
+            tracing::info!("matching all flows");
+            TargetFilter::All
+        }
+        else {
+            tracing::info!("matching: {target:?}");
+            TargetFilter::Set(target.into_iter().collect())
+        });
+
+        socks
+            .builder()?
+            .with_graceful_shutdown(cancel_on_ctrlc_or_sigterm())
+            .with_proxy(fn_proxy(move |incoming, outgoing| {
+                proxy(tls.clone(), filter.clone(), incoming, outgoing)
+            }))
+            .serve()
+            .await?;
+
+        Ok(())
+    }
+}
+
+async fn proxy(
+    tls: tls::Context,
+    filter: Arc<TargetFilter>,
+    incoming: socks::Incoming,
+    outgoing: TcpStream,
+) -> Result<(), skunk::Error> {
+    let target_address = incoming.target_address();
+
+    if filter.matches(target_address) {
+        let span = tracing::info_span!("connection", target = %target_address);
+
+        let is_tls = target_address.port == 443;
+        let (incoming, outgoing) = tls.maybe_decrypt(incoming, outgoing, is_tls).await?;
+
+        http::proxy(incoming, outgoing, |request, send_request| {
+            let span = tracing::info_span!(
+                parent: &span,
+                "request",
+                method = %request.method(),
+                uri = %request.uri()
+            );
+
+            async move {
+                // log request
+                tracing::info!(
+                    parent: &span,
+                    ">"
+                );
+
+                let response = send_request.send(request).await?;
+
+                // log response
+                tracing::info!(
+                    parent: &span,
+                    status = %response.status(),
+                    "<"
+                );
+
+                Ok(response)
+            }
+        })
+        .await?;
+    }
+    else {
+        Passthrough.proxy(incoming, outgoing).await?;
+    };
+
+    Ok::<_, skunk::Error>(())
 }
 
 #[derive(Clone, Debug)]
