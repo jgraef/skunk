aa682c48560d2097747b7e2739a6043cf17f9ead
Janosch Gräf <janosch.graef@gmail.com>
2024-05-24 16:43:42 +0200

    remove graphviz dependency and just write dot manually

--- skunk/src/util/boolean/vis.rs
+++ skunk/src/util/boolean/vis.rs
@@ -1,11 +1,9 @@
-use std::io::Write;
-
-pub use graphviz_rust::dot_structures as dot;
-use graphviz_rust::printer::PrinterContext;
-use petgraph::{
-    graph::NodeIndex,
-    visit::IntoNodeReferences,
+use std::{
+    fmt::Display,
+    io::Write,
 };
+
+use petgraph::visit::IntoNodeReferences;
 
 use super::{
     Graph,
@@ -13,62 +11,53 @@ use super::{
     NodeKind,
 };
 
-fn node_id(node_index: NodeIndex) -> dot::NodeId {
-    dot::NodeId(dot::Id::Anonymous(format!("n{}", node_index.index())), None)
-}
-
-fn node_label(node: &Node) -> dot::Id {
-    dot::Id::Html(match node.kind {
-        NodeKind::Literal(false) => "&#8869;".to_owned(),
-        NodeKind::Literal(true) => "&#8868;".to_owned(),
-        NodeKind::Variable => {
-            node.label
-                .as_ref()
-                .map_or_else(|| "input".to_owned(), |label| format!("input: {label}"))
-        }
-        NodeKind::Not => "&#172;".to_owned(),
-        NodeKind::And => "&#8743;".to_owned(),
-        NodeKind::Or => "&#8744;".to_owned(),
-    })
-}
-
 impl Graph {
-    pub fn to_dot_graph(&self, graph_id: dot::Id) -> dot::Graph {
-        let mut stmts = vec![];
+    /// Writes the graph to `writer` as a [graphviz dot file][1]. This requires
+    /// the `graph-vis` feature to be enabled.
+    ///
+    /// [1]: https://graphviz.org/doc/info/lang.html
+    pub fn write_dot(&self, mut writer: impl Write) -> Result<(), std::io::Error> {
+        writeln!(writer, "strict digraph {{")?;
 
         for (node_index, node) in self.graph.node_references() {
-            stmts.push(dot::Stmt::Node(dot::Node {
-                id: node_id(node_index),
-                attributes: vec![dot::Attribute(
-                    dot::Id::Plain("label".to_owned()),
-                    node_label(node),
-                )],
-            }));
+            writeln!(
+                writer,
+                "  {} [label=<{}>];",
+                node_index.index(),
+                NodeLabel(node),
+            )?;
         }
 
         for edge_index in self.graph.edge_indices() {
             let edge = self.graph.edge_endpoints(edge_index).unwrap();
-            stmts.push(dot::Stmt::Edge(dot::Edge {
-                ty: dot::EdgeTy::Pair(
-                    dot::Vertex::N(node_id(edge.0)),
-                    dot::Vertex::N(node_id(edge.1)),
-                ),
-                attributes: vec![],
-            }));
+            writeln!(writer, "  {} -> {};", edge.0.index(), edge.1.index(),)?;
         }
 
-        dot::Graph::DiGraph {
-            id: graph_id,
-            strict: true,
-            stmts,
-        }
-    }
+        write!(writer, "}}")?;
 
-    pub fn write_dot(&self, mut writer: impl Write) -> Result<(), std::io::Error> {
-        let graph = self.to_dot_graph(dot::Id::Anonymous("graph".to_owned()));
-        let mut context = PrinterContext::default();
-        let graph = graphviz_rust::print(graph, &mut context);
-        writer.write_all(graph.as_bytes())?;
         Ok(())
+    }
+}
+
+/// Helper struct to format node labels
+struct NodeLabel<'a>(&'a Node);
+
+impl<'a> Display for NodeLabel<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.0.kind {
+            NodeKind::Literal(false) => write!(f, "&#8869;"),
+            NodeKind::Literal(true) => write!(f, "&#8868;"),
+            NodeKind::Variable => {
+                if let Some(label) = &self.0.label {
+                    write!(f, "input: {label}")
+                }
+                else {
+                    write!(f, "input")
+                }
+            }
+            NodeKind::Not => write!(f, "&#172;"),
+            NodeKind::And => write!(f, "&#8743;"),
+            NodeKind::Or => write!(f, "&#8744;"),
+        }
     }
 }
