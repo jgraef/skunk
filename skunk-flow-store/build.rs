8476aa09243777f86234c4a84c2c375bc4d13111
Janosch Gräf <janosch.graef@gmail.com>
2024-07-11 05:30:20 +0200

    remove dependency on sqlx command for build script

--- skunk-flow-store/build.rs
+++ skunk-flow-store/build.rs
@@ -1,39 +1,43 @@
-use std::{
-    path::PathBuf,
-    process::Command,
-};
+use std::path::PathBuf;
 
-fn main() {
-    let crate_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
-    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+use sqlx::sqlite::SqliteConnectOptions;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let crate_dir =
+        PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set"));
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").expect("OUT_DIR not set"));
 
     let migrations_dir = crate_dir.join("migrations");
     let db_file = out_dir.join("schema.db");
-    let db_url = format!("sqlite://{}", db_file.display());
 
     println!("cargo::rerun-if-changed={}", migrations_dir.display());
 
+    // delete existing file
     if db_file.exists() {
-        std::fs::remove_file(&db_file).unwrap();
+        std::fs::remove_file(&db_file).unwrap_or_else(|_| {
+            panic!(
+                "Failed to delete existing database file: {}",
+                db_file.display()
+            )
+        });
     }
 
-    std::fs::write(&db_file, b"").unwrap();
+    // create new database
+    let options = SqliteConnectOptions::new()
+        .filename(&db_file)
+        .create_if_missing(true);
+    let pool = sqlx::SqlitePool::connect_with(options)
+        .await
+        .unwrap_or_else(|_| panic!("Could not open database: {}", db_file.display()));
+    sqlx::migrate!("./migrations")
+        .run(&pool)
+        .await
+        .unwrap_or_else(|e| panic!("Failed to run migrations: {e}"));
+    pool.close().await;
 
-    let exit_status = Command::new("sqlx")
-        .arg("migrate")
-        .arg("run")
-        .arg("--source")
-        .arg(&migrations_dir)
-        .arg("--database-url")
-        .arg(&db_url)
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
-
-    if !exit_status.success() {
-        panic!("sqlx failed: {exit_status}");
-    }
-
-    println!("cargo::rustc-env=DATABASE_URL={}", db_url,);
+    println!(
+        "cargo::rustc-env=DATABASE_URL=sqlite://{}",
+        db_file.display()
+    );
 }
