129c93042da892c3398c6e66afb1b403f5879d06
Janosch Gräf <janosch.graef@gmail.com>
2024-07-12 04:01:51 +0200

    wip: reactive config

--- skunk-cli/src/env/config.rs
+++ skunk-cli/src/env/config.rs
@@ -12,6 +12,7 @@ use serde::{
     de::IntoDeserializer,
     Deserialize,
 };
+use skunk_util::trigger;
 use tokio::sync::RwLock;
 use toml_edit::DocumentMut;
 use tracing::Instrument;
@@ -31,10 +32,70 @@ pub struct ConfigFile {
     inner: Arc<RwLock<Inner>>,
 }
 
+
+impl ConfigFile {
+    const DEBOUNCE: Duration = Duration::from_secs(1);
+
+    pub fn open(path: impl AsRef<Path>) -> Result<Loader, Error> {
+        open(path.as_ref())
+    }
+
+    pub async fn get_untracked<T: for<'de> Deserialize<'de>>(
+        &self,
+        key: &str,
+    ) -> Result<Option<T>, Error> {
+        let inner = self.inner.read().await;
+        inner.get_untracked(key)
+    }
+
+    pub async fn get<T: for<'de> Deserialize<'de>>(
+        &self,
+        key: &str
+    ) -> Result<ConfigValue<T>, Error> {
+        let inner = self.inner.read().await;
+        let value = inner.get_untracked(key)?;
+        let changed = inner.changed.clone();
+        Ok(ConfigValue {
+            inner: self.inner.clone(),
+            value,
+            changed,
+        })
+    }
+}
+
+
 pub struct Loader {
     inner: Inner,
     watch: WatchModified,
     hash: FileHash,
+    changed_tx: trigger::Sender,
+}
+
+
+impl Loader {
+    pub fn load(self) -> ConfigFile {
+        let path = self.inner.path.clone();
+        let inner = Arc::new(RwLock::new(self.inner));
+
+        let span = tracing::info_span!("watch-config");
+        tokio::spawn(
+            WatchConfig {
+                inner: inner.clone(),
+                watch: self.watch,
+                path,
+                hash: self.hash,
+                changed: self.changed_tx,
+            }
+            .run()
+            .instrument(span),
+        );
+
+        ConfigFile { inner }
+    }
+
+    pub fn data_dir(&self) -> Result<Option<PathBuf>, Error> {
+        self.inner.get_untracked::<PathBuf>("data_dir")
+    }
 }
 
 #[derive(Debug)]
@@ -42,6 +103,7 @@ struct Inner {
     path: PathBuf,
     hash: FileHash,
     document: DocumentMut,
+    changed: trigger::Receiver,
 }
 
 impl Inner {
@@ -70,46 +132,6 @@ impl Inner {
     }
 }
 
-impl ConfigFile {
-    const DEBOUNCE: Duration = Duration::from_secs(1);
-
-    pub fn open(path: impl AsRef<Path>) -> Result<Loader, Error> {
-        open(path.as_ref())
-    }
-
-    pub async fn get_untracked<T: for<'de> Deserialize<'de>>(
-        &self,
-        key: &str,
-    ) -> Result<Option<T>, Error> {
-        let inner = self.inner.read().await;
-        inner.get_untracked(key)
-    }
-}
-
-impl Loader {
-    pub fn load(self) -> ConfigFile {
-        let path = self.inner.path.clone();
-        let inner = Arc::new(RwLock::new(self.inner));
-
-        let span = tracing::info_span!("watch-config");
-        tokio::spawn(
-            WatchConfig {
-                inner: inner.clone(),
-                watch: self.watch,
-                path,
-                hash: self.hash,
-            }
-            .run()
-            .instrument(span),
-        );
-
-        ConfigFile { inner }
-    }
-
-    pub fn data_dir(&self) -> Result<Option<PathBuf>, Error> {
-        self.inner.get_untracked::<PathBuf>("data_dir")
-    }
-}
 
 fn open(path: &Path) -> Result<Loader, Error> {
     let mut toml: Option<Cow<'static, str>> = None;
@@ -163,15 +185,26 @@ fn open(path: &Path) -> Result<Loader, Error> {
         }
     })?;
 
+    let (changed_tx, changed_rx) = trigger::new();
+
     Ok(Loader {
         inner: Inner {
             path: path.to_owned(),
             hash,
             document,
+            changed: changed_rx,
         },
         watch,
         hash,
+        changed_tx,
     })
+}
+
+#[derive(Debug)]
+pub struct ConfigValue<T> {
+    inner: Arc<RwLock<Inner>>,
+    value: Option<T>,
+    changed: trigger::Receiver,
 }
 
 #[derive(Debug)]
@@ -180,6 +213,7 @@ struct WatchConfig {
     watch: WatchModified,
     path: PathBuf,
     hash: FileHash,
+    changed: trigger::Sender,
 }
 
 impl WatchConfig {
@@ -187,7 +221,11 @@ impl WatchConfig {
         while let Ok(()) = self.watch.wait().await {
             match self.reload() {
                 Ok(None) => {}
-                Ok(Some(_document)) => todo!(),
+                Ok(Some(document)) => {
+                    let mut inner = self.inner.write().await;
+                    inner.document = document;
+                    self.changed.trigger();
+                },
                 Err(_e) => todo!(),
             }
         }
