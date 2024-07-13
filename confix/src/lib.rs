10f037098959f13993f140d3ff453eabff44cf2f
Janosch Gräf <janosch.graef@gmail.com>
2024-07-13 08:23:42 +0200

    wip

--- confix/src/lib.rs
+++ confix/src/lib.rs
@@ -1,1 +1,20 @@
+use toml_edit::{DocumentMut, Value};
 
+
+pub struct ConfigFile {
+
+}
+
+struct Inner {
+    document: DocumentMut,
+}
+
+
+
+pub trait FromToml: Sized {
+    fn from_toml(value: &Value) -> Result<Self, Error>;
+}
+
+pub enum Error {
+
+}
